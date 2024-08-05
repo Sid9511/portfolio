f49a78850fb5189497d9903dc9832259f91391d1
Sid9511 <104961838+Sid9511@users.noreply.github.com>
2024-08-05 07:22:32 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,42 +2,22 @@ import { withSentryConfig } from '@sentry/nextjs';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Enables static export
-  basePath: '/portfolio', // Replace with your repository name
-  assetPrefix: '/portfolio/',
-  // Add any other configurations you need here
+    basePath: '/portfolio', // Replace with your repository name
+    assetPrefix: '/portfolio/',
+    output: 'export', // Ensure this is set for static export
+    experimental: {
+      // Add experimental settings if needed
+      appDir: true, // Example for experimental features
+    },
 };
 
+
 export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
-  org: "self-nn2",
-  project: "my-portfolio",
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-side errors will fail.
-  // tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-});
+    org: "self-nn2",
+    project: "my-portfolio",
+    silent: !process.env.CI,
+    widenClientFileUpload: true,
+    hideSourceMaps: true,
+    disableLogger: true,
+    automaticVercelMonitors: true,
+  });
