c637ba5bdd85443dd3ca296d097de673459fbf36
Sid9511 <104961838+Sid9511@users.noreply.github.com>
2024-08-05 07:17:57 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import { withSentryConfig } from '@sentry/nextjs';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Enables static export
   basePath: '/portfolio', // Replace with your repository name
   assetPrefix: '/portfolio/',
   // Add any other configurations you need here
